09031873f3db3f21f3cbe18f0e3d9b8c604c8cf9
YahyaAzzam <96234204+YahyaAzzam@users.noreply.github.com>
2021-12-19 23:39:04 +0200

    مرة واحد أصلع اتزحلق وقع على دماغه اتزحلق تاني

--- main.c
+++ main.c
@@ -2,12 +2,10 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <math.h>
+<<<<<<< HEAD
+=======
 
-
-
-
-
-
+>>>>>>> 0e91cc00a777b98291a01c74dbbc7c0c47bc7e05
 int playerturn = 0;
 void printworld(int rows,int cols,char array[rows][cols]){
     for(int i=0;i<rows;i++){
@@ -41,7 +39,7 @@ int upperright(int n1,int m1,int rows,int cols,char array[rows][cols]){
                 }
             }
     }
-                                                    return 0;
+   return 0;
 }
 int upperleft(int n1,int m1,int rows,int cols,char array[rows][cols]){
     if((m1 == 0) || (n1 == 0) || array[n1-1][m1-1] == '1')
