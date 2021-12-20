04a965e2e1bc1fd29cc1c4458cce076ff22bfcc4
Hesham-Youssef <96203482+Hesham-Youssef@users.noreply.github.com>
2021-12-20 09:04:25 +0200

    حصل خير
    كل خير انشاء الله

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <math.h>
-int playerturn = 0, player = 1,moves[2] = {0};
+int playerturn = 0, player = 1,moves[2] = {0},totalmoves=0;
 void black(){
     printf("\033[1;30m");
 }
@@ -34,6 +34,19 @@ void normal(){
     printf("\033[0m");
 }
 
+void printhistory(int dim,int array[2 * (dim/2) * ((dim/2) + 1)][7]){
+    for(int i=0;i<2 * (dim/2) * ((dim/2) + 1);i++){
+        for(int j=0;j<7;j++){
+            printf("  %d -",array[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+
+
+
 void printworld(int dim,char array[dim][dim]){
     for(int i=0;i<dim;i++){
         for(int j=0;j<dim;j++){
@@ -52,6 +65,24 @@ void createworld(int dim,char array[dim][dim]){
         }
     }
 }
+void createhistory(int dim,int history[2 * (dim/2) * ((dim/2) + 1)][7]){
+    for(int i=0;i<2 * (dim/2) * ((dim/2) + 1);i++){
+        for(int j=0;j<7;j++){
+            history[i][j] = 0;
+        }
+    }
+}
+void writehistory(int n1,int m1,int n2,int m2,int points0,int points1,int player,int totalmoves,int dim,int history[2 * (dim/2) * ((dim/2) + 1)][7]){
+    history[totalmoves][0] = n1;
+    history[totalmoves][1] = m1;
+    history[totalmoves][2] = n2;
+    history[totalmoves][3] = m2;
+    history[totalmoves][4] = points0;
+    history[totalmoves][5] = points1;
+    history[totalmoves][6] = player;
+}
+
+
 
 int upperright(int n1,int m1,int dim,char array[dim][dim]){
     if((m1 == dim -1) || (n1 == 0) || array[n1-1][m1+1] == 'X' || array[n1-1][m1+1] == 'O')
@@ -170,18 +201,26 @@ void checkforsquares(int n1,int m1,int dim,char array[dim][dim],int points[]){
 
 }
 
-void makeamove(int dim,char array[dim][dim],int n1,int m1,int n2,int m2,int points[]){
+void makeamove(int dim,char array[dim][dim],int n1,int m1,int n2,int m2,int points[],int history[2 * (dim/2) * ((dim/2) + 1)][7]){
     int N1,M1,N2,M2;
     printf("enter the row: ");
     scanf("%d",&N1);
     printf("enter the col: ");
     scanf("%d",&M1);
-
     printf("enter the row: ");
     scanf("%d",&N2);
     printf("enter the col: ");
     scanf("%d",&M2);
+
+    if(N1 == -1 && M1 == -1 && N2 == -1 && M2 == -1){
+        redo(history);
+        return;
+    }
+
+
     n1 = 2*N1-2;m1 = 2*M1-2;n2 = 2*N2-2;m2 = 2*M2-2;
+
+
     if((array[(n1+n2)/2][(m1+m2)/2] =='1') || (n1 > dim) || (m1 > dim) || (n2 > dim) || (m2 > dim) ||(n1 < 0) || (m1 < 0) || (n2 < 0) || (m2 < 0)){
         printf("enter a valid move\n");
         return;
@@ -199,6 +238,19 @@ void makeamove(int dim,char array[dim][dim],int n1,int m1,int n2,int m2,int poin
 
     checkforsquares(n1,m1,dim,array,points);
 
+    writehistory(N1,M1,N2,M2,points[0],points[1],player,totalmoves,dim,history);
+
+    totalmoves++;
+
+}
+
+void redo(){
+
+
+
+
+
+
 
 }
 
@@ -210,41 +262,69 @@ void makeamove(int dim,char array[dim][dim],int n1,int m1,int n2,int m2,int poin
 
 
 
-
-
 int main()
 {
-    int dim,points[2] = {0};
-    int game;
+    int dim=0,points[2] = {0};
+    int game=0;
+    do{
     printf("Welcome to dots and boxes by RABSOOO team\nNew game(1)\nLoad game(2)\nLeader board(3)\nSettings(4)\nExit(5)\n");
     scanf("%d", &game);
     system("cls");
     switch(game){
         case 1:{
             printf("Enter difficulty:\nEasy(1)\nNormal(2)\nHard(3)\n");
+            do{
             scanf("%d", &game);
-            switch(game){
-                case 1:
-                    dim = 7;
-                    break;
-                case 2:
-                    dim = 11;
-                    break;
-                case 3:
-                    dim = 15;
-                    break;
-            }
+                switch(game){
+                    case 1:
+                        dim = 7;
+                        break;
+                    case 2:
+                        dim = 11;
+                        break;
+                    case 3:
+                        dim = 15;
+                        break;
+                    default:
+                        printf("\nenter a valid input: ");
+
+                }
+            }while(dim == 0);
+
+            int history[2 * (dim/2) * ((dim/2) + 1)][7];
             char world[dim][dim];
+
+
             createworld(dim,world);
+
+
+            createhistory(dim,history);
+
+
             do{
             system("cls");
+            printhistory(dim,history);
+            printf("\n\n\n\n");
             printworld(dim,world);
+
             printf("\nFirst player: %d\n\nSecond player: %d\n\nFirst player moves: %i\n\nSecond player moves: %i\n\nTurn player no.: %d\n\n",points[0],points[1],moves[0],moves[1],player);
-            makeamove(dim,world,NULL,NULL,NULL,NULL,points);
+
+            printf("\n enter -1,-1,-1,-1 to redo\n\n");
+
+
+
+            makeamove(dim,world,NULL,NULL,NULL,NULL,points,history);
+
+            printworld(dim,history);
 
             }while((moves[0] + moves[1]) < 2 * (dim/2) * (dim/2 + 1));
+
             system("cls");
+
             printworld(dim,world);
+
+
+
             printf("\nFirst player: %d\n\nSecond player: %d\n\nFirst player moves: %i\n\nSecond player moves: %i\n\nTurn player no.: %d\n\n",points[0],points[1],moves[0],moves[1],player);
             if(points[1]>points[0])
                 printf("Congratulation for player no. 2 and hard luck for player no. 1");
@@ -258,8 +338,12 @@ int main()
         case 3:
         case 4:
         case 5:
+            printf("thanks for playing  : ^)\n");
             break;
+        default:
+            printf("Enter a valid input\n\n");
     }
+    }while(game < 1 || game >5);
 
     /*char world[dim][dim];
     createworld(dim,world);
