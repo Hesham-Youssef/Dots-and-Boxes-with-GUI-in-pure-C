65d520bce46945cb4b513ba892c05669598b0ab0
YahyaAzzam <96234204+YahyaAzzam@users.noreply.github.com>
2022-01-10 13:38:20 +0200

    Music change

--- main.c
+++ main.c
@@ -51,7 +51,7 @@ void initSDL(){
 
     Mix_OpenAudio(44100,MIX_DEFAULT_FORMAT,2,2048);
 
-    backgroundmusic = Mix_LoadMUS("Erik Satie _ Once Upon A Time In Paris (Artwork by Edouard Leon Cortes).ogg");
+    backgroundmusic = Mix_LoadMUS("GameSound.mp3");
 
     window = SDL_CreateWindow("dots and boxes",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,realwidth,height,SDL_WINDOW_SHOWN);
 
